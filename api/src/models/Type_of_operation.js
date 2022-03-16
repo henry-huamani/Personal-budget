d697d0ed763967e7efa03065b7c835e6114cadbb
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-03-15 19:02:40 -0500

    Changes possible values in the name column of the model Type_of_operation

--- api/src/models/Type_of_operation.js
+++ api/src/models/Type_of_operation.js
@@ -3,7 +3,7 @@ const {DataTypes} = require('sequelize');
 module.exports = sequelize => {
     sequelize.define('Type_of_operation', {
         name: {
-            type: DataTypes.ENUM('ingreso', 'egreso'),
+            type: DataTypes.ENUM('income', 'outflow'),
             unique: true
         }
     }, {
