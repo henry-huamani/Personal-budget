b448263ce454d464cad0931dc879397cdfcc3a73
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-03-16 21:24:29 -0500

    Create path to filter last records

---
+++ api/src/routes/index.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const {Operation, Type_of_operation} = require('../db');
+
+router.get('/', async(req, res) => {
+    const {last} = req.query;
+
+    if(last && isNaN(parseInt(last)) === false){
+        const operations = await Operation.findAll({
+            include: Type_of_operation,
+            order: [
+                ['id', 'DESC']
+            ],
+            limit: last,
+        });
+        operations.sort((a, b) => a.id - b.id);
+        return res.send(operations);
+    }
+
+    const operations = await Operation.findAll({
+        include: Type_of_operation,
+        order: [
+            ['id']
+        ],
+    });
+    res.send(operations);
+});
+
+module.exports = {
+    operations: require('./operations'),
+    index: router
+}
