4be57fad77e64bb54669b27c897410693a2bbae3
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-03-17 22:30:29 -0500

    fix(api): renames an attribute of the Operation model

--- api/src/models/Operation.js
+++ api/src/models/Operation.js
@@ -2,7 +2,7 @@ const {DataTypes} = require('sequelize');
 
 module.exports = sequelize => {
     sequelize.define('Operation', {
-        content: {
+        concept: {
             type: DataTypes.STRING,
             allowNull: false
         },
