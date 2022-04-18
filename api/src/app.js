77c5c24707b227edecc7943c857e397dd5e45505
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-04-18 17:12:20 -0500

    fix(api): configures the CORS

--- api/src/app.js
+++ api/src/app.js
@@ -6,6 +6,13 @@ const server = express();
 
 server.use(express.json());
 server.use(morgan('dev'));
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+    next();
+  });
 server.use('/', index);
 server.use('/operations', operations);
 server.use((err, req, res, next) => {
