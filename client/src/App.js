19988535b196e63131e69dd36637e58ca2aaaf72
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-04-21 12:59:52 -0500

    feat(client): shows components in the /home path

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import './App.css';
+import {Route} from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Resultant from './components/Resultant';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Personal Budget</h1>
-    </div>
+    <React.Fragment>
+      <Route path="/home" component={NavBar} />
+      <Route exact path="/home" component={Resultant} />
+    </React.Fragment>
   );
 }
 
