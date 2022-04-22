fd16c7c3a879697c0aba269ce095fac9311eefeb
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-04-22 11:37:34 -0500

    feat(client): shows components in the /home/operations path

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import {Route} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Resultant from './components/Resultant';
+import OperationsFilter from './components/OperationsFilter';
 
 function App() {
   return (
     <React.Fragment>
       <Route path="/home" component={NavBar} />
       <Route exact path="/home" component={Resultant} />
+      <Route exact path="/home/operations" component={OperationsFilter} />
     </React.Fragment>
   );
 }
