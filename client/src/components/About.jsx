b16f14c73b3fd140d15bee6e023b58b7f7e6401d
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-05-07 22:34:22 -0500

    feat(client): adds styles to the About component

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,22 +1,39 @@
 import React from "react";
 import Footer from "./Footer";
 
+import Card from 'react-bootstrap/Card';
+import Carousel from 'react-bootstrap/Carousel';
+import Container from 'react-bootstrap/Container';
+
 const About = () => {
     return(
         <React.Fragment>
-            <header>
-                <h4>Record all your monetary operations to be in control of your finances</h4>
-            </header>
-
-            <section>
-                <img src="/assets/about.png" alt="About" />
-            </section>
-
-            <section>
-                <p>This web app allows you to keep track of the expenses and income of money that you or your family is having.</p>
-                <p>Record with exact date the expenses or income of money to then visualize the result of all of them or also visualize them filtering them by the type of operation.</p>
-                <p>Also edit or delete operations if you made a mistake when registering them or because they are no longer relevant.</p>
-            </section>
+            <Container className="p-1">
+                <Card>
+                    <Card.Header as='h5'>Record all your monetary operations to be in control of your finances</Card.Header>
+                    <Card.Body>
+                        <Carousel className="bg-primary">
+                            <Carousel.Item>
+                                <img className="mx-auto d-block" src="/assets/first.jpg" alt="First slide" style={{width: '20rem', height: '20rem'}}/>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="mx-auto d-block" src="/assets/second.jpg" alt="Second slide" style={{width: '20rem', height: '20rem'}}/>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="mx-auto d-block" src="/assets/third.jpg" alt="Third slide" style={{width: '20rem', height: '20rem'}}/>
+                            </Carousel.Item>
+                            <Carousel.Item>
+                                <img className="mx-auto d-block" src="/assets/fourth.png" alt="Fourth slide" style={{width: '20rem', height: '20rem'}}/>
+                            </Carousel.Item>
+                        </Carousel>
+                        <Card.Text className="mt-4">
+                            <p>This web app allows you to keep track of the expenses and income of money that you or your family is having.</p>
+                            <p>Record with exact date the expenses or income of money to then visualize the result of all of them or also visualize them filtering them by the type of operation.</p>
+                            <p>Also edit or delete operations if you made a mistake when registering them or because they are no longer relevant.</p>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Container>
 
             <Footer/>
         </React.Fragment>
