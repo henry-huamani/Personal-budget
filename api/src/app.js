89574a1ed7285c73682136fb0066f05292968923
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-05-30 21:16:24 -0500

    feat(api): adds the authorization header and adds the authentication paths to the web service

--- api/src/app.js
+++ api/src/app.js
@@ -1,19 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
-const {index, operations} = require('./routes');
+const {index, operations, authentication} = require('./routes');
 
 const server = express();
 
 server.use(express.json());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Authorization, Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+  }
+);
 server.use('/', index);
+server.use('/', authentication);
 server.use('/operations', operations);
 server.use((err, req, res, next) => {
     console.log(err);
