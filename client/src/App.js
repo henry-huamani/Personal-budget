a77382a41f712455019669c0f113149726e450d7
HENRY HUAMANI <huamanihenry22@gmail.com>
2022-05-31 16:57:11 -0500

    feat(client): it adds protection to the views

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
-import {Route} from 'react-router-dom';
+import {Route, Redirect} from 'react-router-dom';
 import NavBar from './components/NavBar';
 import Resultant from './components/Resultant';
 import OperationsFilter from './components/OperationsFilter';
 import About from './components/About';
+import Home from './components/Home';
 
 function App() {
   return (
     <React.Fragment>
-      <Route path="/home" component={NavBar} />
-      <Route exact path="/home" component={Resultant} />
-      <Route exact path="/home/operations" component={OperationsFilter} />
-      <Route exact path="/home/about" component={About} />
+      <Route exact path="/" render={() => document.cookie ? <Redirect to="/home" /> : <Home/>} />
+      <Route path="/home" render={() => document.cookie ? <NavBar/> : <Redirect to="/" />} />
+      <Route exact path="/home" render={() => document.cookie ? <Resultant/> : <Redirect to="/" />} />
+      <Route exact path="/home/operations" render={() => document.cookie ? <OperationsFilter/> : <Redirect to="/" />} />
+      <Route exact path="/home/about" render={() => document.cookie ? <About/> : <Redirect to="/" />} />
     </React.Fragment>
   );
 }
